ecaac6dadc9232388918bead5098c95d333fb2a9
Matt Plichta <bigcatplichta@gmail.com>
2019-09-29 01:19:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,18 +45,27 @@ class TicTacToe
     @board[input] != " " ? true : false #does this ternary have correct syntax? Pipe or double pipe?
   end 
   
+<<<<<<< HEAD
   def valid_move?(index)
+=======
+  def valid_move?(input)
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
     #check that position is valid (part of board array)
     #if above is true, use position_taken? to check that index is available
     #if all good, then true 
     
+<<<<<<< HEAD
     index.between?(0,8) && !position_taken?(index)
+=======
+    @board[input] && !position_taken?(input)
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
   
   def turn 
     #ask for input from user (ex: "Choose your square using 1-9")
     #receive input from user and use that to place token on board
     #use previous methods to define this method 
+<<<<<<< HEAD
     
     print "Choose your square (1-9): "
     input = gets.chomp.to_i
@@ -68,6 +77,22 @@ class TicTacToe
     else 
       turn 
     end 
+=======
+    while !valid_move?(input)
+      print "Choose your square (1-9): "
+      input = gets.chomp.to_i
+      
+      # if input > 0 && input < 10
+        input_to_index(input) 
+      # end 
+      if valid_move?(input)
+        move(input-1, current_player) 
+        display_board
+      else 
+        "That spot is invalid. Please choose another square (1-9): "
+      end 
+    end
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
   
   def turn_count
@@ -88,6 +113,7 @@ class TicTacToe
     winning_combo = nil 
     
     WIN_COMBINATIONS.each do |combo|
+<<<<<<< HEAD
       if @board.values_at(combo[0], combo[1], combo[2]).all?("X")
         winning_combo = combo 
       elsif @board.values_at(combo[0], combo[1], combo[2]).all?("O")
@@ -95,28 +121,45 @@ class TicTacToe
       end
     end 
     winning_combo
+=======
+      if @board.values_at(combo[0], combo[1], combo[2]).all?("X"||"O")
+        winning_combo = combo 
+      end
+    end 
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
     
   def full?
     #check if board is full or has empty spaces
     
+<<<<<<< HEAD
     @board.all? {|e| e != " "} 
+=======
+    @board.all? {|e| e != ""}
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
   
   def draw?
     #if board is full and there are no winning combinations, game is a draw
+<<<<<<< HEAD
     
     full? && !won? 
+=======
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
   
   def over?
     #game is over is board has winning combinations or is full
+<<<<<<< HEAD
     
     won? || draw? 
+=======
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
   
   def winner
     #display winner once a winning combination is met (return X or O as winner)
+<<<<<<< HEAD
     winner = nil
     WIN_COMBINATIONS.each do |combo|
       if @board.values_at(combo[0], combo[1], combo[2]).all?("X")
@@ -126,10 +169,13 @@ class TicTacToe
       end 
     end
     winner
+=======
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
   
   def play 
     #use all previous methods to define game loop and play 
+<<<<<<< HEAD
     
     until over?
       turn
@@ -141,5 +187,7 @@ class TicTacToe
       puts "Cat's Game!"
     end 
     
+=======
+>>>>>>> 2216185e189b0394c0238e61a8997d648d915271
   end 
 end 
